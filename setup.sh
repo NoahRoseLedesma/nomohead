25a8f36a4c2fbb26e95bafdeb5b4c73668419e7c
noahroseledesma <roseledesma@ucdavis.edu>
2019-01-29 05:54:55 +0000

    Create a version of nomohead for local ip

--- setup.sh
+++ setup.sh
@@ -1,27 +1,12 @@
 #!/bin/bash
 echo "Creating config file..."
 
-echo "Enter the location of ngrok: "
-read ngrok_loc
-
 echo "Enter the Dweet ID for IP Address: "
 read d_id_ip
 
-echo "Enter the Dweet ID for Tunnel (leave empty to use the same as IP)"
-read d_id_tun
-
-if [ "$dweet_id_tunnel" = "" ]; then
-	d_id_tun=$d_id_ip
-fi
-
-echo "Enter the delay between posting IP and querying tunnel (Empty for default of 1m): "
-read tun_delay
-
-if [ "$tun_delay" = "" ]; then
-	tun_delay="1m"
-fi
-
-printf "ngrok_location=${ngrok_loc}\ndweet_id_ip=${d_id_ip}\ndweet_id_tunnel=${d_id_tun}\ntunnel_delay=${tun_delay}" > config.cfg
+echo "Enter the interface you would like the IP of dweeted: "
+read interface_id
+printf "dweet_id_ip=${d_id_ip}\ninterface=${interface_id}" > config.cfg
 
 echo "Adding Cron job..."
 
